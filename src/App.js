910b9302f8b0d8399e0109eecfe17c35530ea667
Ivo Krůza <ivo.kruzaa@gmail.com>
2024-11-23 11:27:10 +0100

    implemented basic russian roulette

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+const randomInteger = (max) => Math.floor(Math.random() * max);
+const cylinderSize = 6;
+// const playerCount = 2;
+
+const BOOM = () => <h1> BOOM!!! </h1>;
+const Turn = ({ /*activePlayer,*/ setCylinderAngle }) => (
+  <div>
+    <p> jeď {/* activePlayer */} </p>
+    <button onClick={() => {
+      setCylinderAngle((previousAngle) => (previousAngle + 1) % cylinderSize);
+    }}> strelit </button>
+  </div>
+);
 
 function App() {
+  // const [activePlayer, setActivePlayer] = useState(0);
+  const [cylinderAngle, setCylinderAngle] = useState(randomInteger(cylinderSize));
+  const [bulletPosition, setBulletPosition] = useState(randomInteger(cylinderSize));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          prdil tatkaEdit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {cylinderAngle === bulletPosition
+      ? <BOOM/>
+      : <Turn /*activePlayer={activePlayer}*/ setCylinderAngle={setCylinderAngle} />
+    }
     </div>
   );
 }
